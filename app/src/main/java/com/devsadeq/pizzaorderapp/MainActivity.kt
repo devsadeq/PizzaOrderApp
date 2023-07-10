29e8ff5528e0baf02ed71e52941f533dd1536ecc
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-10 22:29:17 +0300

    setup

--- app/src/main/java/com/devsadeq/pizzaorderapp/MainActivity.kt
+++ app/src/main/java/com/devsadeq/pizzaorderapp/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.devsadeq.pizzaorderapp.ui.screen.OrderScreen
 import com.devsadeq.pizzaorderapp.ui.theme.PizzaOrderAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    OrderScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PizzaOrderAppTheme {
-        Greeting("Android")
     }
 }
