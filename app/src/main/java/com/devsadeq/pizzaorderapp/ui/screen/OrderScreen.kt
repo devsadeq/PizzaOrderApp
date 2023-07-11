1ef5dc0ca90ea26bbd036742bb80a115e8bd8b3e
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-11 04:41:28 +0300

    create order screen ui

--- app/src/main/java/com/devsadeq/pizzaorderapp/ui/screen/OrderScreen.kt
+++ app/src/main/java/com/devsadeq/pizzaorderapp/ui/screen/OrderScreen.kt
@@ -1,17 +1,61 @@
 package com.devsadeq.pizzaorderapp.ui.screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.devsadeq.pizzaorderapp.ui.composable.OrderScreenTopBar
+import com.devsadeq.pizzaorderapp.ui.composable.PizzaIngredients
+import com.devsadeq.pizzaorderapp.ui.composable.PizzaPager
+import com.devsadeq.pizzaorderapp.ui.composable.PizzaPrice
+import com.devsadeq.pizzaorderapp.ui.composable.PizzaSizeSelection
+import com.devsadeq.pizzaorderapp.viewmodel.OrderScreenUiState
+import com.devsadeq.pizzaorderapp.viewmodel.OrderScreenViewModel
+import com.devsadeq.pizzaorderapp.viewmodel.PizzaSize
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OrderScreen() {
-    OrderScreenContent()
+fun OrderScreen(viewModel: OrderScreenViewModel = hiltViewModel()) {
+    val state by viewModel.state.collectAsState()
+    Scaffold(
+        topBar = { OrderScreenTopBar(state.isFavorite, viewModel::onFavoriteClicked) },
+    ) { paddingValues ->
+        OrderScreenContent(
+            state,
+            paddingValues,
+            viewModel::onPizzaSizeClicked,
+            viewModel::onIngredientClicked
+        )
+    }
 }
 
 @Composable
-private fun OrderScreenContent() {
-    Text(text = "Order Screen")
+private fun OrderScreenContent(
+    state: OrderScreenUiState,
+    paddingValues: PaddingValues,
+    onPizzaSizeClicked: (PizzaSize) -> Unit,
+    onIngredientClicked: (OrderScreenUiState.Ingredient) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        PizzaPager(pizzaList = state.pizzaList)
+        PizzaPrice(price = state.totalPrice)
+        PizzaSizeSelection(selectedSize = state.selectedSize, onClick = onPizzaSizeClicked)
+        PizzaIngredients(ingredients = state.ingredients, onIngredientClicked = onIngredientClicked)
+    }
 }
 
 @Preview
